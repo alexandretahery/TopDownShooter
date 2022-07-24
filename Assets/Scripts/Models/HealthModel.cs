1f429bf733bb62c738595882dc09b9d5b213a5d5
alexandre1 tahery <alexandre1.tahery@epitech.eu>
2022-07-24 12:25:10 +0200

    [Health System] Bar Canvas added

---
+++ Assets/Scripts/Models/HealthModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class HealthModel
+{
+    private float health;
+    private EnumStatus enumStatus;
+
+    /// <summary>
+    /// Property Set or Get in percentage Health
+    /// </summary>
+    public float Health { get { return (float)health; } set { health = value; } }
+
+    /// <summary>
+    /// Property Set or Get EnumStatus
+    /// </summary>
+    public EnumStatus EnumStatus { get { return enumStatus; } set { enumStatus = value; } }
+    /// <summary>
+    /// CTOR for health system
+    /// </summary>
+    /// <param name="health">Give health into range between 0 to 100</param>
+    /// <param name="enumStatus">Give health status</param>
+    public HealthModel(float health, EnumStatus enumStatus)
+    {
+        if (health < 0)
+        this.health = 0;
+        else if (health >= 1)
+            this.health = 1;
+        this.enumStatus = EnumStatus.Normal;
+    }
+}
